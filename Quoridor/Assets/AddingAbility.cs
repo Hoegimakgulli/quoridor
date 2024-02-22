5cb68beb048201b0d6d859c21115f45c10b761d1
GyubinLee399 <127742771+GyubinLee399@users.noreply.github.com>
2024-02-22 23:25:46 +0900

    플레이어 능력 추가 버튼 생성

---
+++ Quoridor/Assets/AddingAbility.cs
@@ -0,0 +1,23 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddingAbility : MonoBehaviour
+{
+    private Text inputText;
+    //private GameObject player;
+    private PlayerAbility playerAbility;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //inputText = transform.GetChild(2).GetComponent<Text>();
+        playerAbility = GameObject.FindWithTag("Player").GetComponent<PlayerAbility>();
+    }
+
+    public void AddAbility()
+    {
+        inputText = transform.GetChild(2).GetComponent<Text>();
+        playerAbility.AddAbility(int.Parse(inputText.text));
+    }
+}
