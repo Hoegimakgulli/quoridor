1580a5798da564c247ab5eeeb9aa6d7626b91c19
superjoung <asss6868@naver.com>
2024-03-12 16:37:28 +0900

    V0.4.0
    이미지를 제외하고 능력 창 열기 성공

--- Quoridor/Assets/Scripts/AbilitySelect.cs
+++ Quoridor/Assets/Scripts/AbilitySelect.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AbilityDescription
 {
@@ -68,6 +69,7 @@ public class AbilitySelect : MonoBehaviour
     {
         AbilityStart(); // skills 딕셔너리에 값 대입
         SlotStart();    // slots 딕셔너리에 값 대입
+        ShareAbility();
     }
 
     public void Start()
@@ -116,45 +118,45 @@ public class AbilitySelect : MonoBehaviour
         skills.Add(3, new AbilityDescription("벽 소지 + 1", "벽을 1채 더 세운다.", 0));
         skills.Add(4, new AbilityDescription("벽 소지 + 2", "벽을 2채 더 세운다.", 0));
         skills.Add(5, new AbilityDescription("벽 소지 + 3", "벽을 3채 더 세운다.", 1));
-        skills.Add(6, new AbilityDescription("보호막", "공격력을 막는 보호막을 얻는다. <color=orange>한 번</color> 막으면 동료를 후방으로 밀고 사라진다.", 2));
-        skills.Add(7, new AbilityDescription("질긴 생존", "죽음을 극복한다. <color=orange>한 번</color> 사망을 3턴 미루고 적을 전멸시키면 부활한다.", 2));
-        skills.Add(8, new AbilityDescription("재장전", "공격으로 적을 처치하면 한 번 더 공격할 기회를 잡는다.", 0));
-        skills.Add(9, new AbilityDescription("체인 라이트닝", "공격받은 적이 쓰러질 때 근처 적에게 전격을 넘겨 피해를 1 입힌다.", 0));
-        skills.Add(10, new AbilityDescription("연쇄 폭파", "공격받은 적이 쓰러질 때 주변 지대를 폭파해 피해를 1 입힌다.", 0));
-        skills.Add(11, new AbilityDescription("관통 공격", "공격으로 적을 처치하면 바로 뒤의 적에게도 피해를 1 입힌다.", 0));
-        skills.Add(12, new AbilityDescription("정밀 공격", "한 턴 동안 공격력이 1 증가한다.", 1));
-        skills.Add(13, new AbilityDescription("연막탄", "2턴 유지되는 <color=yellow>연막 지대</color>를 만든다. 연막 지대의 적은 공격하지 못하고 이동할 방향을 잃는다.", 0));
-        skills.Add(14, new AbilityDescription("독성 폭탄", "2턴 유지되는 <color=yellow>독 지대</color>를 만든다. 지대에 드는 적은 행동력 2를 잃고 머무르는 동안 피해를 입는다.", 1));
-        skills.Add(15, new AbilityDescription("수류탄", "범위 내 적에게 상당한 피해를 입힌다.", 2));
+        skills.Add(6, new AbilityDescription("보호막", "공격력을 막는 보호막을 얻는다. <color=orange>한 번</color> 막으면\n동료를 후방으로 밀고 사라진다.", 2));
+        skills.Add(7, new AbilityDescription("질긴 생존", "죽음을 극복한다. <color=orange>한 번</color> 사망을 3턴 미루고\n적을 전멸시키면 부활한다.", 2));
+        skills.Add(8, new AbilityDescription("재장전", "공격으로 적을 처치하면\n한 번 더 공격할 기회를 잡는다.", 0));
+        skills.Add(9, new AbilityDescription("체인 라이트닝", "공격받은 적이 쓰러질 때\n근처 적에게 전격을 넘겨 피해를 1 입힌다.", 0));
+        skills.Add(10, new AbilityDescription("연쇄 폭파", "공격받은 적이 쓰러질 때\n주변 지대를 폭파해 피해를 1 입힌다.", 0));
+        skills.Add(11, new AbilityDescription("관통 공격", "공격으로 적을 처치하면\n바로 뒤의 적에게도 피해를 1 입힌다.", 0));
+        skills.Add(12, new AbilityDescription("정밀 공격", "한 턴 동안\n공격력이 1 증가한다.", 1));
+        skills.Add(13, new AbilityDescription("연막탄", "2턴 유지되는 <color=yellow>연막 지대</color>를 만든다.\n연막 지대의 적은 공격하지 못하고\n이동할 방향을 잃는다.", 0));
+        skills.Add(14, new AbilityDescription("독성 폭탄", "2턴 유지되는 <color=yellow>독 지대</color>를 만든다.\n지대에 드는 적은 행동력 2를 잃고\n머무르는 동안 피해를 입는다.", 1));
+        skills.Add(15, new AbilityDescription("수류탄", "범위 내 적에게\n상당한 피해를 입힌다.", 2));
         skills.Add(16, new AbilityDescription("투척 대미지 증가 1", "투척물의 피해량이 1 증가한다.", 1));
         skills.Add(17, new AbilityDescription("투척 대미지 증가 2", "투척물의 피해량이 1 증가한다.", 2));
         skills.Add(18, new AbilityDescription("투척 사거리 확장 1", "투척물을 더 멀리 던진다.", 0));
         skills.Add(19, new AbilityDescription("투척 사거리 확장 2", "투척물을 더 멀리 던진다.", 0));
         skills.Add(20, new AbilityDescription("투척 개수 증가 1", "투척물을 하나 더 소지한다.", 0));
         skills.Add(21, new AbilityDescription("투척 개수 증가 2", "투척물을 하나 더 소지한다.", 1));
-        skills.Add(22, new AbilityDescription("자동 덫 설치", "5번째 이동마다 <color=teal>덫</color>을 남긴다. 밟은 적은 약간의 체력과 행동력 3을 잃는다.", 0));
-        skills.Add(23, new AbilityDescription("칼날 지뢰 설치", "<color=teal>칼날 지뢰</color>를 설치한다. 밟은 적은 큰 피해를 입는다. <color=red>벽을 넘어 설치할 수 있다.</color>", 2));
-        skills.Add(24, new AbilityDescription("미끌젤리 설치", "<color=teal>젤리 지대</color>를 생성한다. 밟은 적은 넘어지며 경미한 피해를 입고 지대 밖까지 미끄러진다. <color=red>벽을 넘어 설치할 수 있다.</color>", 0));
+        skills.Add(22, new AbilityDescription("자동 덫 설치", "5번째 이동마다 <color=#008080>덫</color>을 남긴다.\n밟은 적은 약간의 체력과\n행동력 3을 잃는다.", 0));
+        skills.Add(23, new AbilityDescription("칼날 지뢰 설치", "<color=#008080>칼날 지뢰</color>를 설치한다.\n밟은 적은 큰 피해를 입는다.\n\n<color=red>벽을 넘어 설치할 수 있다.</color>", 2));
+        skills.Add(24, new AbilityDescription("미끌젤리 설치", "<color=#008080>젤리 지대</color>를 생성한다.\n밟은 적은 넘어지며\n경미한 피해를 입고\n지대 밖까지 미끄러진다.\n\n<color=red>벽을 넘어 설치할 수 있다.</color>", 0));
         skills.Add(25, new AbilityDescription("설치 대미지 증가 1", "설치물의 피해량이 1 증가한다.", 0));
         skills.Add(26, new AbilityDescription("설치 대미지 증가 2", "설치물의 피해량이 1 증가한다.", 1));
         skills.Add(27, new AbilityDescription("설치 개수 증가 1", "설치물을 하나 더 소지한다.", 0));
         skills.Add(28, new AbilityDescription("설치 개수 증가 2", "설치물을 하나 더 소지한다.", 0));
-        skills.Add(29, new AbilityDescription("더미 설치", "<color=teal>분신 더미</color>를 설치한다. 더미는 적을 유인하지만 피격되면 사라진다. <color=red>벽을 넘어 설치할 수 있다.</color>", 1));
-        skills.Add(30, new AbilityDescription("포병 화력", "본대에 포격을 요청한다. 지정한 지점 주변 범위에 피해를 1 입히는 탄을 5발 발사한다. 포격은 <color=red>벽으로 막히지 않는다.</color>", 3));
-        skills.Add(31, new AbilityDescription("저격 요청", "본대에 저격을 요청한다. 지정한 지점에 피해를 1 입히는 탄환을 쏜다. 저격은 <color=red>벽으로 막히지 않는다.</color>", 3));
-        skills.Add(32, new AbilityDescription("정밀 포격", "본대에 포격을 요청한다. 지정한 지점과 십자 범위에 피해를 2 입히는 탄을 5발 폭격한다. 폭격은 <color=red>벽으로 막히지 않는다.</color>", 3));
+        skills.Add(29, new AbilityDescription("더미 설치", "<color=#008080>분신 더미</color>를 설치한다.\n더미는 적을 유인하지만\n피격되면 사라진다.\n\n<color=red>벽을 넘어 설치할 수 있다.</color>", 1));
+        skills.Add(30, new AbilityDescription("포병 화력", "본대에 포격을 요청한다.\n지정한 지점 주변 범위에\n피해를 1 입히는 탄을\n5발 발사한다.\n\n포격은 <color=red>벽으로 막히지 않는다.</color>", 3));
+        skills.Add(31, new AbilityDescription("저격 요청", "본대에 저격을 요청한다.\n지정한 지점에\n피해를 1 입히는 탄환을 쏜다.\n\n저격은 <color=red>벽으로 막히지 않는다.</color>", 3));
+        skills.Add(32, new AbilityDescription("정밀 포격", "본대에 포격을 요청한다.\n지정한 지점과 십자 범위에\n피해를 2 입히는 탄을\n5발 폭격한다.\n\n폭격은 <color=red>벽으로 막히지 않는다.</color>", 3));
         skills.Add(33, new AbilityDescription("회피 기동", "공격 직후 전후좌우 중 한 곳으로 이동한다.", 1));
         skills.Add(34, new AbilityDescription("건설 기동", "건설과 이동을 모두 수행한다.", 0));
         skills.Add(35, new AbilityDescription("빠른 기동", "이동을 두 번 수행한다.", 0));
-        skills.Add(36, new AbilityDescription("넉백", "적이 공격으로 쓰러지지 않으면 한 칸 밀어낸다.", 0));
-        skills.Add(37, new AbilityDescription("위압감", "전장에 진입하면 모든 적이 경직되어 행동력을 3 잃는다.", 1));
-        skills.Add(38, new AbilityDescription("발목 공격", "적이 공격에 맞을 때 행동력을 3 잃게 한다.", 1));
-        skills.Add(39, new AbilityDescription("수감", "적군 한 명을 수감한다. 수감된 적은 2턴 간 행동력을 회복하지 않고 이동하지 못한다.", 2));
-        skills.Add(40, new AbilityDescription("정신조종", "가장 가까운 적을 <color=orange>한 번</color> 조종한다. 조종하는 적이 근처 아군에게 피해 3을 입히게 하고 공격할 수 있는 아군이 없으면 본인이 피해를 입게한다. <color=red>벽은 무시하고</color> 조종할 수 있다.", 3));
-        skills.Add(41, new AbilityDescription("리콜", "즉시 전리품을 다시 배열한다. 주로 수집한 계열을 반드시 한 개 이상 포함한다.", 3));
-        skills.Add(42, new AbilityDescription("약독주", "숙취가 심한 선주를 <color=orange>한 병</color> 얻는다. 2턴 간 약효가 돌아 공격력이 3 증가하고 이후 한 턴은 숙취가 남아 공격력이 1 하락한다.", 3));
-        skills.Add(43, new AbilityDescription("회유", "주변 8칸의 적을 <color=orange>한 번</color> 회유한다. 들은 적은 모두 설득되어 즉시 전장을 떠난다. 회유는 <color=red>벽에 막히지 않는다.</color>", 3));
-        skills.Add(44, new AbilityDescription("수면모래", "전장에 수면 모래를 <color=orange>한 번</color> 뿌린다. 모든 적은 피해를 입기 전까지 꿈속을 헤맨다. 잠든 적은 행동력도 회복하지 않는다. 수면 모래는 <color=red>벽에 막히지 않는다.</color>", 3));
+        skills.Add(36, new AbilityDescription("넉백", "적이 공격으로 쓰러지지 않으면\n한 칸 밀어낸다.", 0));
+        skills.Add(37, new AbilityDescription("위압감", "전장에 진입하면\n모든 적이 경직되어\n행동력을 3 잃는다.", 1));
+        skills.Add(38, new AbilityDescription("발목 공격", "적이 공격에 맞을 때\n행동력을 3 잃게 한다.", 1));
+        skills.Add(39, new AbilityDescription("수감", "적군 한 명을 수감한다.\n수감된 적은 2턴 간\n행동력을 회복하지 않고\n이동하지 못한다.", 2));
+        skills.Add(40, new AbilityDescription("정신조종", "가장 가까운 적을 <color=orange>한 번</color> 조종한다.\n조종하는 적이 근처 아군에게\n피해 3을 입히게 하고\n공격할 수 있는 아군이 없으면\n본인이 피해를 입게한다.\n\n<color=red>벽은 무시하고</color> 조종할 수 있다.", 3));
+        skills.Add(41, new AbilityDescription("리콜", "즉시 전리품을 다시 배열한다.\n주로 수집한 계열을\n반드시 한 개 이상 포함한다.", 3));
+        skills.Add(42, new AbilityDescription("약독주", "숙취가 심한 선주를 <color=orange>한 병</color> 얻는다.\n2턴 간 약효가 돌아\n공격력이 3 증가하고\n이후 한 턴은 숙취가 남아\n공격력이 1 하락한다.", 3));
+        skills.Add(43, new AbilityDescription("회유", "주변 8칸의 적을 <color=orange>한 번</color> 회유한다.\n들은 적은 모두 설득되어\n즉시 전장을 떠난다.\n\n회유는 <color=red>벽에 막히지 않는다.</color>", 3));
+        skills.Add(44, new AbilityDescription("수면모래", "전장에 수면 모래를 <color=orange>한 번</color> 뿌린다.\n모든 적은 피해를 입기 전까지 꿈속을 헤맨다.\n잠든 적은 행동력도 회복하지 않는다.\n\n수면 모래는 <color=red>벽에 막히지 않는다.</color>", 3));
     }
 
     public void SlotStart()
@@ -208,11 +210,12 @@ public class AbilitySelect : MonoBehaviour
             Debug.LogError("AbilitySelect Scripts AbilitySelectStart 함수 : 해당 스테이지에 맞는 slots를 찾지 못했습니다.");
         }
 
-        for(int slotCount = 0; slotCount < 3; slotCount++) // 첫번째 슬롯 두번째 슬롯 세번째 슬롯까지 총 3개 뽑는 과정 실시
+        List<int> tmpSkillNumBox = new List<int>(); // 임시로 뽑힌 능력이 어떤 번호인지 담아두는 변수
+        for (int slotCount = 0; slotCount < 3; slotCount++) // 첫번째 슬롯 두번째 슬롯 세번째 슬롯까지 총 3개 뽑는 과정 실시
         {
             float select = Random.Range(0.0f, 100.0f); // 확률계산
             float sum = 0; // 확률 계산에 필요한 sum 변수
-            float count = 0; // 
+            int count = 0; // 저 중 고 특수중 어떤 등급을 뽑아야하는지 결정하는 float함수
             switch (slotCount) // 각 슬롯에 해당하는 저 중 고 특성 순서대로 비교 연산 
             {
                 case 0:
@@ -253,31 +256,40 @@ public class AbilitySelect : MonoBehaviour
             int skillSelect = 0;
             switch (count) //각 등급에 맞는 능력중에서 리스트로 뽑아 이전에 나온 능력인지 확인 최종 스킬 번호 추출
             {
-                case 0:
+                case 0: // 이미 뽑힌 스킬이 아닐때까지 계속 뽑음
                     do
                     {
                         skillSelect = lowSkills[Random.Range(0, lowSkills.Count)];
-                    } while (!skills[skillSelect].isGet);
+                    } while (skills[skillSelect].isGet && tmpSkillNumBox.Contains(skillSelect));
                     break;
                 case 1:
                     do
                     {
                         skillSelect = middleSkills[Random.Range(0, middleSkills.Count)];
-                    } while (!skills[skillSelect].isGet);
+                    } while (skills[skillSelect].isGet && tmpSkillNumBox.Contains(skillSelect));
                     break;
                 case 2:
                     do
                     {
                         skillSelect = highSkills[Random.Range(0, highSkills.Count)];
-                    } while (!skills[skillSelect].isGet);
+                    } while (skills[skillSelect].isGet && tmpSkillNumBox.Contains(skillSelect));
                     break;
                 case 3:
                     do
                     {
                         skillSelect = specialSkills[Random.Range(0, specialSkills.Count)];
-                    } while (!skills[skillSelect].isGet);
+                    } while (skills[skillSelect].isGet && tmpSkillNumBox.Contains(skillSelect));
                     break;
             }
+
+            tmpSkillNumBox.Add(skillSelect);
+            /*
+             * 스킬 선택창을 열고 중복되는 슬롯에 중복되는 능려이 안뜨게 작성할것
+             */
+            
+            slotUi[slotCount].transform.GetChild(1).GetComponent<TMP_Text>().text = skills[skillSelect].skillName; // 스킬 이름 넣어주는 파트
+            slotUi[slotCount].transform.GetChild(2).GetComponent<TMP_Text>().text = skills[skillSelect].skillDescription; // 스킬 설명 넣어주는 파트
+            GameObject.Find("AbilitySelectPanel").transform.GetChild(0).gameObject.SetActive(true);
         }
     }
 }
