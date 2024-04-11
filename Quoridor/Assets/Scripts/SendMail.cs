1c805e9ae360e17b79a29a307bb47baaef509e29
Dony910 <lovegameoh@gmail.com>
2024-04-11 21:40:45 +0900

    Update SendMail.cs

--- Quoridor/Assets/Scripts/SendMail.cs
+++ Quoridor/Assets/Scripts/SendMail.cs
@@ -38,23 +38,6 @@ public class SendMail : MonoBehaviour
         Application.OpenURL(uri);
 
 
-        // string fileName = GetComponentInChildren<Text>().text;
-        // MailMessage mail = new MailMessage();
-
-        // mail.From = new MailAddress("ydh910.b@gmail.com");
-        // mail.To.Add("ydh910.b@gmail.com");
-        // mail.Subject = fileName;
-        // mail.Body = $"대략적인 상황 설명을 해주세요.\n\n 추가로 이미지나 영상 파일을 첨부해주시면 더욱 빠른 해결이 가능합니다.\n\n감사합니다!";
-        // Attachment attachment = new Attachment(Application.persistentDataPath + "/logs/" + fileName + ".log");
-        // mail.Attachments.Add(attachment);
-        // SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
-        // smtpServer.Port = 587;
-        // smtpServer.Credentials = new System.Net.NetworkCredential("ydh910.b@gmail.com", "bgmb lmnb brta jyky") as ICredentialsByHost;
-        // smtpServer.EnableSsl = true;
-        // ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
-
-        // smtpServer.Send(mail);
-
         transform.parent.parent.GetComponent<SendMail>().OpenPanel();
     }
     bool isOpened = false;
